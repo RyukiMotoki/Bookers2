9fb35d723d6fcc131adac18e2393cb7befc026e7
RyukiMotoki <125866183+RyukiMotoki@users.noreply.github.com>
2023-03-27 19:46:59 +0000

    [Add] ファイルの準備と設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "homes#top"
+  devise_for :users
+  get "/homes/about" => "homes#about"
+  resources :books
+  resources :users, only: [:index, :show, :edit, :update]
 end
