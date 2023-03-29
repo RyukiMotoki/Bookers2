be1463faaea50fd20689d38c8b1fde2dbe9af206
RyukiMotoki <125866183+RyukiMotoki@users.noreply.github.com>
2023-03-29 07:19:03 +0000

    [Add] view、controllerの記述、CRUDの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,38 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
+  def index
+    @user = User.find(current_user.id)
+    @users = User.all
+    @book = Book.new
+  end
+
   def show
+    @book = Book.new
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+
 end
